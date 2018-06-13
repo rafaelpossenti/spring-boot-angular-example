8de929c9126131afdded8c4a9ce5f1af9dc06715
Rafael Possenti <rafael_possenti@hotmail.com>
2018-06-13 20:10:58 -0100

    finish rest call

--- server/src/main/java/com/possenti/demo/beer/BeerController.java
+++ server/src/main/java/com/possenti/demo/beer/BeerController.java
@@ -1,9 +1,11 @@
 package com.possenti.demo.beer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 import java.util.stream.Collectors;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BeerController {
@@ -14,6 +16,7 @@ public class BeerController {
 	}
 
 	@GetMapping("/good-beers")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public Collection<Beer> goodBeers() {
 		return repository.findAll().stream().filter(this::isGreat).collect(Collectors.toList());
 	}
